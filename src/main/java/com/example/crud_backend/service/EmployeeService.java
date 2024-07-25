4c31567624e4b18d7a9a737e746517a351dbb6c0
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-25 17:25:55 +0530

    create method to get employee by id

--- src/main/java/com/example/crud_backend/service/EmployeeService.java
+++ src/main/java/com/example/crud_backend/service/EmployeeService.java
@@ -21,4 +21,8 @@ public class EmployeeService {
         employeeDao.findAll().forEach(employees::add);
         return employees;
     }
+
+    public Employee getEmployeeById(Integer employeeId){
+
+    }
 }
