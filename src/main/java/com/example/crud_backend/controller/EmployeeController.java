1bfb7cf5c4628104005d8e89261db32f4122a83f
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-25 16:56:43 +0530

    add annotation autowired

--- src/main/java/com/example/crud_backend/controller/EmployeeController.java
+++ src/main/java/com/example/crud_backend/controller/EmployeeController.java
@@ -2,13 +2,14 @@ package com.example.crud_backend.controller;
 
 import com.example.crud_backend.entity.Employee;
 import com.example.crud_backend.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
-
+    @Autowired
     private EmployeeService employeeService;
 
     @PostMapping("/save/employee")
