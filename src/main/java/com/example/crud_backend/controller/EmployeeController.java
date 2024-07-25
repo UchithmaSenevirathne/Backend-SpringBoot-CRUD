a3a2e759ee28ebbea50c67298cea62fe39edae33
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-25 16:54:26 +0530

    create save method in employee controller

--- src/main/java/com/example/crud_backend/controller/EmployeeController.java
+++ src/main/java/com/example/crud_backend/controller/EmployeeController.java
@@ -1,7 +1,10 @@
 package com.example.crud_backend.controller;
 
+import com.example.crud_backend.entity.Employee;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
+
+    public void saveEmployee(Employee employee){}
 }
