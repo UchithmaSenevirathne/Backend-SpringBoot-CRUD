497a4bb795647bb5cd63e8bbe79459253642e04a
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-25 16:23:12 +0530

    add getters and setters

--- src/main/java/com/example/crud_backend/entity/Employee.java
+++ src/main/java/com/example/crud_backend/entity/Employee.java
@@ -12,4 +12,60 @@ public class Employee {
 
     public Employee() {
     }
+
+    public Integer getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Integer employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getEmployeeContactNumber() {
+        return employeeContactNumber;
+    }
+
+    public void setEmployeeContactNumber(String employeeContactNumber) {
+        this.employeeContactNumber = employeeContactNumber;
+    }
+
+    public String getEmployeeAddress() {
+        return employeeAddress;
+    }
+
+    public void setEmployeeAddress(String employeeAddress) {
+        this.employeeAddress = employeeAddress;
+    }
+
+    public String getEmployeeGender() {
+        return employeeGender;
+    }
+
+    public void setEmployeeGender(String employeeGender) {
+        this.employeeGender = employeeGender;
+    }
+
+    public String getEmployeeDepartment() {
+        return employeeDepartment;
+    }
+
+    public void setEmployeeDepartment(String employeeDepartment) {
+        this.employeeDepartment = employeeDepartment;
+    }
+
+    public String getEmployeeSkills() {
+        return employeeSkills;
+    }
+
+    public void setEmployeeSkills(String employeeSkills) {
+        this.employeeSkills = employeeSkills;
+    }
 }
