89d5f99c2d8e72a3f713963c3d44190fa005e4fa
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-25 17:00:08 +0530

    add annotation autowired

--- src/main/java/com/example/crud_backend/service/EmployeeService.java
+++ src/main/java/com/example/crud_backend/service/EmployeeService.java
@@ -2,11 +2,12 @@ package com.example.crud_backend.service;
 
 import com.example.crud_backend.dao.EmployeeDao;
 import com.example.crud_backend.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
-
+    @Autowired
     private EmployeeDao employeeDao;
     public void saveEmployee(Employee employee){}
 }
