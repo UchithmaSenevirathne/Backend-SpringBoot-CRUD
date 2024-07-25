eaf8210373097422059ee5ed75dbcb90c50482de
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-25 17:14:50 +0530

    add annotation getmapping

--- src/main/java/com/example/crud_backend/controller/EmployeeController.java
+++ src/main/java/com/example/crud_backend/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.example.crud_backend.controller;
 import com.example.crud_backend.entity.Employee;
 import com.example.crud_backend.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class EmployeeController {
         return employeeService.saveEmployee(employee);
     }
 
+    @GetMapping("/get/employee")
     public void getEmployee(){
 
     }
