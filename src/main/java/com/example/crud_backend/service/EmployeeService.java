2952231c25dd66e20206926419b611c8a9e0a47b
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-25 17:17:26 +0530

    create get method in employeeservice

--- src/main/java/com/example/crud_backend/service/EmployeeService.java
+++ src/main/java/com/example/crud_backend/service/EmployeeService.java
@@ -5,11 +5,17 @@ import com.example.crud_backend.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class EmployeeService {
     @Autowired
     private EmployeeDao employeeDao;
     public Employee saveEmployee(Employee employee){
         return employeeDao.save(employee);
+    }
+
+    public List<Employee> getEmployees(){
+
     }
 }
