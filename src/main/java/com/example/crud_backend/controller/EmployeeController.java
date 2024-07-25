bdfddb0d72720913f69386ce9e557f0ce64483d1
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-25 16:55:43 +0530

    add annotation postmapping

--- src/main/java/com/example/crud_backend/controller/EmployeeController.java
+++ src/main/java/com/example/crud_backend/controller/EmployeeController.java
@@ -1,11 +1,13 @@
 package com.example.crud_backend.controller;
 
 import com.example.crud_backend.entity.Employee;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
 
+    @PostMapping("/save/employee")
     public void saveEmployee(@RequestBody Employee employee){}
 }
