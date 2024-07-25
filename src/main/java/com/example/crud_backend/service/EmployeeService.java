11faf7086768b8497e1b89838a0f06ef79946a56
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-25 16:59:50 +0530

    create referece employee dao

--- src/main/java/com/example/crud_backend/service/EmployeeService.java
+++ src/main/java/com/example/crud_backend/service/EmployeeService.java
@@ -1,10 +1,12 @@
 package com.example.crud_backend.service;
 
+import com.example.crud_backend.dao.EmployeeDao;
 import com.example.crud_backend.entity.Employee;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
 
+    private EmployeeDao employeeDao;
     public void saveEmployee(Employee employee){}
 }
