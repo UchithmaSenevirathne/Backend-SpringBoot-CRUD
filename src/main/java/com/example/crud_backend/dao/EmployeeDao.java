5b1a3685cf0033f819e8da9aed21ac9c2c110435
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-25 16:39:38 +0530

    extend crudrepository to employeedao

--- src/main/java/com/example/crud_backend/dao/EmployeeDao.java
+++ src/main/java/com/example/crud_backend/dao/EmployeeDao.java
@@ -1,7 +1,9 @@
 package com.example.crud_backend.dao;
 
+import com.example.crud_backend.entity.Employee;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeDao {
+public interface EmployeeDao extends CrudRepository<Employee, Integer> {
 }
