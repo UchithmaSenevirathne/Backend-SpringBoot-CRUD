de3b9e0b8b9a72a0739a812b6d8deb1444a16549
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-25 16:54:59 +0530

    add annotation requestBody

--- src/main/java/com/example/crud_backend/controller/EmployeeController.java
+++ src/main/java/com/example/crud_backend/controller/EmployeeController.java
@@ -1,10 +1,11 @@
 package com.example.crud_backend.controller;
 
 import com.example.crud_backend.entity.Employee;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
 
-    public void saveEmployee(Employee employee){}
+    public void saveEmployee(@RequestBody Employee employee){}
 }
