318a85f900e8cc1107ebb5eebcc210a0779be26f
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-25 17:24:20 +0530

    add annotation pathvariable

--- src/main/java/com/example/crud_backend/controller/EmployeeController.java
+++ src/main/java/com/example/crud_backend/controller/EmployeeController.java
@@ -3,10 +3,7 @@ package com.example.crud_backend.controller;
 import com.example.crud_backend.entity.Employee;
 import com.example.crud_backend.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +22,5 @@ public class EmployeeController {
         return employeeService.getEmployees();
     }
     @GetMapping("/get/employee/{employeeId}")
-    public void getEmployeeById(Integer employeeId){}
+    public void getEmployeeById(@PathVariable Integer employeeId){}
 }
