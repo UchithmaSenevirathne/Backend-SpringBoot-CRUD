9b39c9277c4080f0f2161be4e4090ba562b6a52f
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-25 16:59:07 +0530

    create save method in employee service

--- src/main/java/com/example/crud_backend/service/EmployeeService.java
+++ src/main/java/com/example/crud_backend/service/EmployeeService.java
@@ -1,7 +1,10 @@
 package com.example.crud_backend.service;
 
+import com.example.crud_backend.entity.Employee;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
+
+    public void saveEmployee(Employee employee){}
 }
