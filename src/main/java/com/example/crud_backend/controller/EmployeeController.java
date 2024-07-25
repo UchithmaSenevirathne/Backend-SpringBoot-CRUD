9a8ab234868dca6ef4192c9e721057b7c234b16a
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-25 16:56:20 +0530

    create referece employee service

--- src/main/java/com/example/crud_backend/controller/EmployeeController.java
+++ src/main/java/com/example/crud_backend/controller/EmployeeController.java
@@ -1,12 +1,15 @@
 package com.example.crud_backend.controller;
 
 import com.example.crud_backend.entity.Employee;
+import com.example.crud_backend.service.EmployeeService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
+
+    private EmployeeService employeeService;
 
     @PostMapping("/save/employee")
     public void saveEmployee(@RequestBody Employee employee){}
